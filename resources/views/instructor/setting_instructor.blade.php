603eeb4007241d6169abfe1c19368cbe8b34b719
yasmin <yasmin@git>
2020-01-05 14:23:31 +0200

    add instructor files

---
+++ resources/views/instructor/setting_instructor.blade.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html lang="ar">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width , initial-scale=1 , shrink-to-fit=no"/>
+    <!-- Bootstrap CSS & js -->
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css"/>
+
+    <title>setting instructor</title>
+    <style>
+        body {
+            direction: rtl;
+            margin-top: 50px;
+            font-family: 'GESSTwoLight-Light' !important;
+        }
+    </style>
+</head>
+<body class="bg-light">
+
+
+<!-- Begin Page Content -->
+<section>
+    <div class="container-fluid   text-right">
+
+        <div class="row">
+            <div class="col-lg-2"></div>
+            <div class="col-lg-8">
+                <div class="card mb-4">
+                    <div class="card-header text-primary">
+                        تعديل البايانات الشخصيه
+                    </div>
+                    <div class="card-body">
+                        <form action="" method="post">
+                            <div class="row">
+                                <div class="col-sm text-center ">
+                                    <h5> email@email.com</h5>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="form-row form-group">
+                                <div class=" col-sm-6   ">
+                                    <div class="form-row form-group">
+                                        <div class="col-4 ">الاسم باللغه العربيه</div>
+
+                                        <div class="col-8 ">
+                                            <input type="text" name="" value=" ندي " class='form-control'>
+                                        </div>
+                                    </div>
+                                    <hr>
+
+                                </div>
+                                <div class=" col-sm-6 ">
+                                    <div class="form-row form-group">
+                                        <div class="col-4 ">الاسم باللغه الانجليزيه</div>
+
+                                        <div class="col-8 ">
+                                            <input type="text" name="" value="nada " class='form-control'>
+                                        </div>
+                                    </div>
+                                    <hr>
+
+                                </div>
+                            </div>
+                            <div class="form-row form-group">
+                                <div class="col-sm-6 ">
+                                    <div class="form-row form-group">
+                                        <div class="col-4 ">المؤهل الدراسى</div>
+
+                                        <div class="col-8 ">
+                                            <select name="degree" class="form-control">
+                                                <option>طالب</option>
+                                                <option>خريج</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <hr>
+
+                                </div>
+                                <div class="col-sm-6 ">
+                                    <div class="form-row form-group">
+                                        <div class="col-4 ">الكليه</div>
+
+                                        <div class="col-8 ">
+                                            <select name="faculty" class="form-control">
+                                                <option>هندسه</option>
+                                                <option>تجاره</option>
+                                            </select>
+
+                                        </div>
+                                    </div>
+                                    <hr>
+
+                                </div>
+                            </div>
+                            <div class="form-row form-group">
+
+                                <div class="col-sm-6 ">
+                                    <div class="form-row form-group">
+                                        <div class="col-4 "> رقم التليفون المحمول</div>
+
+                                        <div class="col-8 ">
+                                            <input type="text" name="" value="  " class='form-control'>
+                                        </div>
+                                    </div>
+                                    <hr>
+
+                                </div>
+                                <div class="col-sm-6 ">
+                                    <div class="form-row form-group">
+                                        <div class="col-4 "> تليفون اخر</div>
+
+                                        <div class="col-8 ">
+                                            <input type="text" name="" value="  " class='form-control'>
+                                        </div>
+                                    </div>
+                                    <hr>
+
+                                </div>
+
+
+                            </div>
+
+                            <div class="form-row form-group">
+                                <div class="col-sm-6 ">
+                                    <div class="form-row form-group">
+                                        <div class="col-4 "> البلد</div>
+
+                                        <div class="col-8 ">
+                                            <input type="text" name="" value="  " class='form-control'>
+                                        </div>
+                                    </div>
+                                    <hr>
+
+                                </div>
+                                <div class="col-sm-6 ">
+                                    <div class="form-row form-group">
+                                        <div class="col-4 ">المدينه</div>
+
+                                        <div class="col-8 ">
+                                            <input type="text" name="" value=" " class='form-control'>
+                                        </div>
+                                    </div>
+                                    <hr>
+
+                                </div>
+                            </div>
+                            <div class="form-row form-group">
+                                <div class="col-2 ">العنوان</div>
+
+                                <div class="col-10">
+                                            <textarea name="address" placeholder="ادخل العنوان " rows="2"
+                                                      class="form-control" value=" "></textarea>
+                                </div>
+                                <hr>
+                            </div>
+                            <div class="form-row form-group">
+                                <div class="col-2 ">نبذه عن</div>
+
+                                <div class="col-10">
+                                            <textarea name="about" placeholder="نبذه عن " rows="3"
+                                                      class="form-control"
+                                                      style="  overflow-scrolling:auto; "></textarea>
+                                </div>
+                                <hr>
+                            </div>
+
+
+                            <div class="form-row save">
+                                <div class="col-sm-6 mx-auto text-center">
+                                    <button class="btn btn-primary" type="submit" id="submit">حفظ</button>
+                                    <button class="btn  btn-danger" type="reset"> الغاء</button>
+                                </div>
+                            </div>
+                            <br>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /.container-fluid -->
+
+
+</section>
+<!-- script-->
+<script type="text/javascipt" src="/js/jQuery.js"></script>
+<script type="text/javascript" src="/js/bootstrap.bundle.min.js"></script>
+<script src="/js/jquery-3.3.1.min.js"></script>
+<script src="/js/popper.min.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+
+
+</body>
+</html>
+
+
+
+
+
