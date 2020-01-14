1e5d0b68e2f573d444608ee1408fe35bc5eb1a85
ibrahimothman <ibrahimothmanre@gmail.com>
2020-01-14 15:51:32 +0200

    feat : show rooms

--- resources/views/rooms/rooms_view.blade.php
+++ resources/views/rooms/rooms_view.blade.php
@@ -7,21 +7,41 @@
     <link rel="stylesheet" href="/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/room_style.css">
     <title> view rooms</title>
+
+
+    <link href="/../../../css/styles.css" rel="stylesheet">
+    <!-- Custom fonts for this template-->
+
+    <link href="{{url('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="{{url('css/sb-admin-rtl.css')}}" rel="stylesheet">
+    <link
+        rel="stylesheet"
+        href="https://cdn.rtlcss.com/bootstrap/v4.2.1/css/bootstrap.min.css"
+        integrity="sha384-vus3nQHTD+5mpDiZ4rkEPlnkcyTP+49BhJ4wJeJunw06ZAp+wzzeBPUXr42fi8If"
+        crossorigin="anonymous">
 </head>
 <body class="bg-light">
+<div id="wrapper">
+    @include('sidebar')
+    <div id="content-wrapper" class="d-flex flex-column">
+    @include('operationBar')
 <!-- Begin Page Content -->
+
 <div class="container text-center ">
     <div class="row">
         <div class=" col">
             <div class="card text-center">
                 <div class="card-body">
-                    <a class="add btn btn-outline-primary" href="#" type="button"> اضافه غرفه جديده</a>
+                    <a class="add btn btn-outline-primary" href="{{ route('rooms.create') }}" type="button"> اضافه غرفه جديده</a>
                 </div>
             </div>
         </div>
     </div>
 </div>
-<div class="container addRoom text-right px-5 py-5">
+<div class="container addRoom  px-5 py-5">
     <div class="row">
         <div class="col">
             <h2 class="text-primary ">الغرف</h2>
@@ -30,83 +50,18 @@
     </div>
     <!-- card -->
     <div class="row">
-        <div class=" col-md-3  col-xs-1">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-text">غرفه رقم 1</h5>
-                    <a href="#" class="text-primary">قراء المزيد</a>
+        @foreach($rooms as $room)
+            <div class=" col-md-3  col-xs-1">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-text">{{ $room->name }}1</h5>
+                        <a href="#" class="text-primary">قراء المزيد</a>
+                    </div>
                 </div>
+                <br>
             </div>
-            <br>
-        </div>
-        <div class=" col-md-3  col-xs-1">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-text">غرفه رقم 1</h5>
-                    <a href="#" class="text-primary">قراء المزيد</a>
-                </div>
-            </div>
-            <br>
-        </div>
-        <div class=" col-md-3  col-xs-1">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-text">غرفه رقم 1</h5>
-                    <a href="#" class="text-primary">قراء المزيد</a>
-                </div>
-            </div>
-            <br>
-        </div>
-        <div class=" col-md-3  col-xs-1">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-text">غرفه رقم 1</h5>
-                    <a href="#" class="text-primary">قراء المزيد</a>
-                </div>
-            </div>
-            <br>
-        </div>
-    </div>
-    <br>
-    <div class="row">
-        <div class=" col-md-3  col-xs-1">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-text">غرفه رقم 1</h5>
-                    <a href="#" class="text-primary">قراء المزيد</a>
-                </div>
-            </div>
-            <br>
-        </div>
-        <div class=" col-md-3  col-xs-1">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-text">غرفه رقم 1</h5>
-                    <a href="#" class="text-primary">قراء المزيد</a>
-                </div>
-            </div>
-            <br>
-        </div>
-        <div class=" col-md-3  col-xs-1">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-text">غرفه رقم 1</h5>
-                    <a href="#" class="text-primary">قراء المزيد</a>
-                </div>
-            </div>
-            <br>
-        </div>
-        <div class=" col-md-3  col-xs-1">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-text">غرفه رقم 1</h5>
-                    <a href="#" class="text-primary">قراء المزيد</a>
-                </div>
-            </div>
-            <br>
-        </div>
-    </div>
-    <br>
+        @endforeach
+
 </div>
 <!-- script-->
 <script type="text/javascipt" src="/js/jQuery.js"></script>
