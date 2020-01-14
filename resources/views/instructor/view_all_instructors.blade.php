8a77266167125455fbf72732c38c1766f84e0022
ahmdkhled <ahmdkhled@gmail.com>
2020-01-15 01:55:13 +0200

    feat:implement show instructors feature

--- resources/views/instructor/view_all_instructors.blade.php
+++ resources/views/instructor/view_all_instructors.blade.php
@@ -27,12 +27,12 @@
         <ul class="navbar-nav text-right">
 
             <li class="nav-item ml-4">
-                <h5 style="color: #007bff">تصنيف علي حسب</h5>
+                <h5 hidden style="color: #007bff">تصنيف علي حسب</h5>
 
             </li>
 
             <li class=" nav-item  dropdown ml-4 ">
-                <button data-toggle="dropdown" class="dropdown-toggle btn-primary py-1">
+                <button hidden data-toggle="dropdown" class="dropdown-toggle btn-primary py-1">
                     الاقسام <b class="caret"></b>
                 </button>
                 <ul class=" dropdown-menu text-right">
@@ -44,7 +44,7 @@
             </li>
             <br>
             <li class=" nav-item  dropdown ml-4 ">
-                <button data-toggle="dropdown" class="dropdown-toggle btn-primary py-1">
+                <button hidden data-toggle="dropdown" class="dropdown-toggle btn-primary py-1">
                     المواعيد <b class="caret"></b>
                 </button>
                 <ul class=" dropdown-menu text-right">
@@ -73,60 +73,24 @@
         </div>
     </div>
     <!-- card -->
+
+    @for($i=0;$i<count($instructors);$i+=3)
     <div class="row">
-
+        @for($j=$i;$j<$i+3;$j++)
+            @if($j==count($instructors))
+                @break
+                @endif
         <div class="col-xs-12 col-sm-6 col-md-4">
             <div class="image-flip">
                 <div class="mainflip">
                     <div class="frontside">
                         <div class="card motionCard">
                             <div class="card-body text-center">
-                                <p><img class="img-fluid rounded-circle  " src="\img\portfoliopic_0.jpg" width="100"
+                                <p><img class="img-fluid rounded-circle  " src="{{$instructors[$j]->image==null?$Constants->getInstructorPlaceholderImage:$instructors[$j]->image}}" width="100"
                                         height="100" alt="card image"></p>
-                                <h4 class="card-title">اسم المدرب</h4>
-                                <p class="card-text">هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي
-                                    القارئ </p>
-                                <a href="#" class="btn btn-primary btn-sm">قراءه المزيد</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <br>  
-        </div>
-
-        <div class="col-xs-12 col-sm-6 col-md-4">
-            <div class="image-flip">
-                <div class="mainflip">
-                    <div class="frontside">
-                        <div class="card motionCard">
-                            <div class="card-body text-center">
-                                <p><img class="img-fluid rounded-circle  " src="\img\portfoliopic_0.jpg" width="100"
-                                        height="100" alt="card image"></p>
-                                <h4 class="card-title">اسم المدرب</h4>
-                                <p class="card-text">هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي
-                                    القارئ</p>
-                                <a href="#" class="btn btn-primary btn-sm">قراءه المزيد</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <br>
-        </div>
-
-        <div class="col-xs-12 col-sm-6 col-md-4">
-            <div class="image-flip">
-                <div class="mainflip">
-                    <div class="frontside">
-                        <div class="card motionCard">
-                            <div class="card-body text-center">
-                                <p><img class="img-fluid rounded-circle  " src="\img\portfoliopic_0.jpg" width="100"
-                                        height="100" alt="card image"></p>
-                                <h4 class="card-title">اسم المدرب</h4>
-                                <p class="card-text">هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي
-                                    القارئ</p>
-                                <a href="#" class="btn btn-primary btn-sm">قراءه المزيد</a>
+                                <h4 class="card-title">{{$instructors[$j]->nameAr}}</h4>
+                                <p class="card-text">{{$instructors[$j]->bio}}</p>
+                                <a href="/instructor/{{$instructors[$j]->id}}" class="btn btn-primary btn-sm">قراءه المزيد</a>
                             </div>
                         </div>
                     </div>
@@ -136,77 +100,12 @@
             </div>
             <br>
         </div>
+            @endfor
     </div>
-
-    <div class="row">
-
-        <div class="col-xs-12 col-sm-6 col-md-4">
-            <div class="image-flip">
-                <div class="mainflip">
-                    <div class="frontside">
-                        <div class="card motionCard">
-                            <div class="card-body text-center">
-                                <p><img class="img-fluid rounded-circle  " src="\img\portfoliopic_0.jpg" width="100"
-                                        height="100" alt="card image"></p>
-                                <h4 class="card-title">اسم المدرب</h4>
-                                <p class="card-text">هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي
-                                    القارئ </p>
-                                <a href="#" class="btn btn-primary btn-sm">قراءه المزيد</a>
-                            </div>
-                        </div>
-                    </div>
-
-
-                </div>
-            </div>
-            <br>
-        </div>
-
-        <div class="col-xs-12 col-sm-6 col-md-4">
-            <div class="image-flip">
-                <div class="mainflip">
-                    <div class="frontside">
-                        <div class="card motionCard">
-                            <div class="card-body text-center">
-                                <p><img class="img-fluid rounded-circle  " src="\img\portfoliopic_0.jpg" width="100"
-                                        height="100" alt="card image"></p>
-                                <h4 class="card-title">اسم المدرب</h4>
-                                <p class="card-text">هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي
-                                    القارئ</p>
-                                <a href="#" class="btn btn-primary btn-sm">قراءه المزيد</a>
-                            </div>
-                        </div>
-                    </div>
-
-
-                </div>
-            </div>
-            <br>
-        </div>
-
-        <div class="col-xs-12 col-sm-6 col-md-4">
-            <div class="image-flip">
-                <div class="mainflip">
-                    <div class="frontside">
-                        <div class="card motionCard">
-                            <div class="card-body text-center">
-                                <p><img class="img-fluid rounded-circle  " src="\img\portfoliopic_0.jpg" width="100"
-                                        height="100" alt="card image"></p>
-                                <h4 class="card-title">اسم المدرب</h4>
-                                <p class="card-text">هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي
-                                    القارئ</p>
-                                <a href="#" class="btn btn-primary btn-sm">قراءه المزيد</a>
-                            </div>
-                        </div>
-                    </div>
-
-
-                </div>
-            </div>
-            <br>
-        </div>
-    </div>
+    @endfor
+{!! $instructors->render() !!}
 </div>
+
         @include('footer')
     </div>
 </div>
