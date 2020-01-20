b34c3d9cdedfb455507a687492136b23cddddbac
yasmin <yasmin@git>
2020-01-21 00:31:30 +0200

    add client side validation of test enrollment folder

--- resources/views/testTakes/test-take.blade.php
+++ resources/views/testTakes/test-take.blade.php
@@ -1,74 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>test-take</title>
-
-    <!-- Custom fonts for this template-->
-    <link href="{{url('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="{{url('css/sb-admin-rtl.css')}}" rel="stylesheet">
-    <link href="{{url('employee')}}" rel="stylesheet">
-    <link
-        rel="stylesheet"
-        href="https://cdn.rtlcss.com/bootstrap/v4.2.1/css/bootstrap.min.css"
-        integrity="sha384-vus3nQHTD+5mpDiZ4rkEPlnkcyTP+49BhJ4wJeJunw06ZAp+wzzeBPUXr42fi8If"
-        crossorigin="anonymous">
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script
-        src="https://cdn.rtlcss.com/bootstrap/v4.2.1/js/bootstrap.min.js"
-        integrity="sha384-a9xOd0rz8w0J8zqj1qJic7GPFfyMfoiuDjC9rqXlVOcGO/dmRqzMn34gZYDTel8k"
-        crossorigin="anonymous"></script>
+    <!-- Bootstrap CSS & js -->
+    @include('library')
 
     <!-- Custom styles for this page -->
     <link href="{{url('vendor/datatables/datatables.min.css')}}" rel="stylesheet">
 
     <link href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" rel="stylesheet">
     @inject('Utility', 'App\Utility')
+    <title>test-take</title>
 
 </head>
 
 <body id="page-top">
 
-<!-- Page Wrapper -->
 <div id="wrapper">
-
-   @include('sidebar')
-
-    <!-- Content Wrapper -->
+    @include('sidebar')
     <div id="content-wrapper" class="d-flex flex-column">
-
+    @include('operationBar')
         <!-- Main Content -->
         <div id="content">
-
-            <!-- Topbar -->
-            <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-                <!-- Sidebar Toggle (Topbar) -->
-                <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3"> <i class="fa fa-bars"></i> </button>
-
-                <!-- Topbar Search -->
-                <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-                    <div class="input-group">
-                        <input type="text" class="form-control bg-light border-0 small" placeholder="البحث عن " aria-label="Search" aria-describedby="basic-addon2">
-                        <div class="input-group-append">
-                            <button class="btn btn-primary" type="button"> <i class="fas fa-search fa-sm"></i> </button>
-                        </div>
-                    </div>
-                </form>
-
-                @include('tobbar')
-            <!-- End of Topbar -->
-
-            <!-- Begin Page Content -->
-
             <!-- Page Heading -->
 
             <div class="container-fluid">
@@ -80,16 +32,12 @@
                             </div>
                         </div>
                         <div class="card-body">
-
-
-
-
                             <div class=" clearfix col-md-8 mb-4">
 
-                                <form method="get" action="{{url("test-takes/create")}}">
+                                <form method="get" action="{{url("test-takes/create")}}" >
                                     <div class="form-row col-md-12 ">
                                         <select name="test"  id="testselector"     class="form-control ">
-                                            <option value="0"> اختر الامتحان</option>
+                                            <option value=""> اختر الامتحان</option>
                                             @foreach($testGroups as $testGroup)
                                                 <option value="{{$testGroup->id}}">{{$testGroup->name}}</option>
 
@@ -98,11 +46,6 @@
                                     </div>
                                 </form>
                             </div>
-
-
-
-
-
 
                             <!-- stu-block -->
 
@@ -124,11 +67,7 @@
             <!-- End of Main Content -->
 
             <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto"> <span>Copyright &copy;وحدة التعليم الالكترونى - جامعه المنصورة </span> </div>
-                </div>
-            </footer>
+        @include('footer')
             <!-- End of Footer -->
 
         </div>
@@ -155,16 +94,9 @@
         </div>
     </div>
 </div>
+<!-- script-->
 
-<!-- Bootstrap core JavaScript-->
-<script src="{{url('vendor/jquery/jquery.min.js')}}"></script>
-<script src="{{url('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-
-<!-- Core plugin JavaScript-->
-<script src="{{url('vendor/jquery-easing/jquery.easing.min.js')}}"></script>
-
-<!-- Custom scripts for all pages-->
-<script src="{{url('js/sb-admin-2.min.js')}}"></script>
+@include('script')
 <!-- Page level plugins -->
 <script src="{{url('vendor/datatables/datatables.min.js')}}"></script>
 <!-- Page level custom scripts -->
