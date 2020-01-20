0a9c3de6132aaf3cf90f3f79d58fc46c10ed5f87
yasmin <yasmin@git>
2020-01-20 22:05:42 +0200

    add client side validation of test folder

--- resources/views/operationBar.blade.php
+++ resources/views/operationBar.blade.php
@@ -1,16 +1,18 @@
 <div id="content" class="operations-bar">
-    <header>
-        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-            <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3"> <i class="fa fa-bars"></i> </button>
-            <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-                <div class="input-group">
-                    <input type="text" class="form-control bg-light border-0 small" placeholder="البحث عن " id="search" aria-label="Search" aria-describedby="basic-addon2">
-                    <div class="input-group-append">
-                        <button class="btn btn-primary" type="button"> <i class="fas fa-search fa-sm"></i> </button>
-                    </div>
+
+    <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+        <!-- Sidebar Toggle (Topbar) -->
+        <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3"> <i class="fa fa-bars"></i> </button>
+        <!-- Topbar Search -->
+        <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+            <div class="input-group">
+                <input type="text" class="form-control bg-light border-0 small" placeholder="البحث عن " aria-label="Search" aria-describedby="basic-addon2">
+                <div class="input-group-append">
+                    <button class="btn btn-primary" type="button"> <i class="fas fa-search fa-sm"></i> </button>
                 </div>
-            </form>
-            @include('tobbar')
-        </nav>
-    </header>
-</div>
+            </div>
+        </form>
+
+    @include('tobbar')
+
+
--- resources/views/script.blade.php
+++ resources/views/script.blade.php
@@ -1,10 +1,7 @@
-<script type="text/javascript" src="/js/jQuery.js"></script>
+
 <script type="text/javascript" src="/js/bootstrap.bundle.min.js"></script>
-<script src="/js/jquery-3.3.1.min.js"></script>
 <script src="/js/popper.min.js"></script>
 <script src="/js/bootstrap.min.js"></script>
-
-
 <!-- Bootstrap core JavaScript-->
 <script src="{{url('vendor/jquery/jquery.min.js')}}"></script>
 <script src="{{url('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
@@ -14,3 +11,6 @@
 <script src="{{url('js/sb-admin-2.min.js')}}"></script>
 <script type='text/javascript'
         src="https://rawgit.com/RobinHerbots/jquery.inputmask/3.x/dist/jquery.inputmask.bundle.js"></script>
+
+
+
