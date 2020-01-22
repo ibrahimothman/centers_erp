b11a4ab2a5b26642acebba6bdbb3b1ad107ca689
yasmin <yasmin@git>
2020-01-22 18:02:57 +0200

    add client side validation of courses folder

--- resources/views/courses/viewCourses.blade.php
+++ resources/views/courses/viewCourses.blade.php
@@ -3,26 +3,9 @@
     <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
         <head>
-            <meta charset="utf-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-            <meta name="description" content="">
-            <meta name="author" content="">
-
-            <title>Available Course</title>
-
-            <!-- Custom fonts for this template-->
-            <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-            <link href="/../../../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-
-            <!-- Custom styles for this template-->
-            <link href="/../../../css/sb-admin-rtl.css" rel="stylesheet">
+            @include('library')
             <link href="/../../../css/courses-styles.css" rel="stylesheet">
-            <link rel="stylesheet" href="https://cdn.rtlcss.com/bootstrap/v4.2.1/css/bootstrap.min.css" integrity="sha384-vus3nQHTD+5mpDiZ4rkEPlnkcyTP+49BhJ4wJeJunw06ZAp+wzzeBPUXr42fi8If" crossorigin="anonymous">
-
-            <!-- Latest compiled and minified JavaScript -->
-            <script src="https://cdn.rtlcss.com/bootstrap/v4.2.1/js/bootstrap.min.js" integrity="sha384-a9xOd0rz8w0J8zqj1qJic7GPFfyMfoiuDjC9rqXlVOcGO/dmRqzMn34gZYDTel8k" crossorigin="anonymous"></script>
-            <script src="https://kit.fontawesome.com/58b9d7bcbd.js" crossorigin="anonymous"></script>
+            <title>Available Course</title>
         </head>
 
         <body>
@@ -105,20 +88,8 @@
                     @include('footer')
                 </div>
             </div>
-
-
-        <!-- Bootstrap core JavaScript-->
-        <script src="{{url('vendor/jquery/jquery.min.js')}}"></script>
-        <script src="{{url('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-        <!-- Core plugin JavaScript-->
-        <script src="{{url('vendor/jquery-easing/jquery.easing.min.js')}}"></script>
-        <!-- Custom scripts for all pages-->
-        <script src="{{url('js/sb-admin-2.min.js')}}"></script>
-        <script type='text/javascript' src="https://rawgit.com/RobinHerbots/jquery.inputmask/3.x/dist/jquery.inputmask.bundle.js"></script>
-        <script>
-
-
-        </script>
+            <!-- script-->
+            @include('script')
     </body>
 
 </html>
