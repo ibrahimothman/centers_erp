b271ffe5ac6817fd9b28b1b7e772f3dc0bcd9712
ibrahimothman <ibrahimothmanre@gmail.com>
2020-01-22 10:29:15 +0200

    fix : set students/create as home_page

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::resource('centers','CentersController');
 Route::resource('employees','EmployeeController');
 
 // --------------------- students --------------------
-Route::get('/', 'HomeController@index' );
+Route::get('/', 'StudentController@create' );
 Route::resource('students','StudentController');
 
 Route::get('students.table','StudentController@showTable')->name('students.table');
