08d4baf3276737d00fb6a3cb2fed1fb2c33d8b1f
ibrahimothman <ibrahimothmanre@gmail.com>
2020-01-23 13:40:28 +0200

    feat : add api/students

---
+++ app/Http/Resources/Student.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Student extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return[
+            'nameAr' => $this->nameAr,
+            'nameEn' => $this->nameEn,
+            'email' => $this->email,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
