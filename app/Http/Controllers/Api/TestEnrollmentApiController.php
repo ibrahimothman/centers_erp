fad8264a1b861b15a89c20b2e5f24d31913385d7
ibrahimothman <ibrahimothmanre@gmail.com>
2020-01-25 18:21:56 +0200

    feat : add test_enrollment/show of a student

--- app/Http/Controllers/Api/TestEnrollmentApiController.php
+++ app/Http/Controllers/Api/TestEnrollmentApiController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Api;
 
 use App\Student;
+use App\Http\Resources\TestGroup as TestGroupResource;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 
 class TestEnrollmentApiController extends Controller
 {
+
+
     public function store(Request $request){
         $enrollment_data = $this->validateRequest($request);
         if($enrollment_data->fails()){
@@ -19,6 +22,11 @@ class TestEnrollmentApiController extends Controller
         $student->testsEnrolling()->syncWithoutDetaching($enrollment_data->validate()['test_group_id']);
         return response()->json(['message' => 'Successfully you have enrolled in this group'], 200);
     }
+
+    public function show($student){
+        return TestGroupResource:: collection(Student::findOrFail($student)->testsEnrolling);
+    }
+
 
     private function validateRequest(Request $request){
         return Validator::make($request->all(),[
