72ff81b95a81a56164cf90a7eb8af90744cd236f
yasmin <yasmin@git>
2020-02-11 02:25:57 +0200

    add landing page

---
+++ resources/views/landingPage/landingPage.blade.php
@@ -0,0 +1,207 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Edumark</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSS here -->
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <!-- icon -->
+    <link rel="stylesheet" href="/css/font-awesome.min.css">
+    <!-- style page-->
+    <link rel="stylesheet" href="/css/landingPageStyle.css">
+    <style>
+    </style>
+</head>
+
+<body>
+<!-- nav bar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+    <a class="navbar-brand" href="#">
+        <h3 class="text-light">ITC Academy</h3>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto ">
+            <li class="nav-item active">
+                <a class="nav-link text-light" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-light " href="#">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-light" href="#">Contact</a>
+            </li>
+            <li class="nav-item text-danger">
+                <a class="nav-link text-light login popup-with-form  " href="#test-form">Log in</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<!-- slider_area_start -->
+<div class="slider_area ">
+    <div class="single_slider d-flex align-items-center justify-content-center slider_bg_1">
+        <div class="container">
+            <div class="row align-items-center justify-content-center">
+                <div class="col-xl-6 col-md-6">
+                    <div class="illastrator_png">
+                        <img src="\img\edu_ilastration.png" alt="">
+                    </div>
+                </div>
+                <div class="col-xl-6 col-md-6">
+                    <div class="slider_info">
+                        <h3>Log In <br>
+                            Admin Panel <br>
+                            Now</h3>
+                        <a href="#test-form" class=" login popup-with-form boxed_btn">Log In Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- slider_area_end -->
+<!-- content -->
+<div class="site-section mt-5 mb-5" >
+    <div class="container">
+        <div class="row mb-5">
+            <div class="col-12 text-center">
+                <h2 class="section-title mb-3">About Us</h2>
+            </div>
+        </div>
+        <div class="row mb-5">
+            <div class="col-lg-5 ml-auto pl-lg-5">
+                <h2 class="text-black mb-4 h3 font-weight-bold">Our Mission</h2>
+                <p class="mb-4">Eos cumque optio dolores excepturi rerum temporibus magni recusandae eveniet, totam omnis consectetur maxime quibusdam expedita dolorem dolor nobis dicta labore quaerat esse magnam unde, aperiam delectus! At maiores, itaque.</p>
+                <ul class="ul-check mb-5 list-unstyled success">
+                    <li>Laborum enim quasi at modi</li>
+                    <li>Ad at tempore</li>
+                    <li>Labore quaerat esse</li>
+                </ul>
+                <a href="" class=" learnMore mb-5">learn more</a>
+            </div>
+            <div class="col-lg-6" data-aos="fade-right">
+                <img src="img\undraw_posting_photo.svg" alt="Image" class="img-fluid">
+            </div>
+        </div>
+    </div>
+</div>
+<!--  end content -->
+<!-- footer -->
+<footer class="footer footer_bg_1 py-5 mt-5 ">
+    <div class="footer_top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6  mt-lg-0 mt-sm-5  mt-4">
+                    <h3 class="text-light mb-3">     About</h3>
+                    <p>
+                        Our set he for firmament morning sixth subdue darkness creeping gathered divide our let god moving. Moving in fourth air night bring upon you’re it beast let you dominion likeness open place day great wherein heaven sixth lesser subdue fowl
+                    </p>
+                </div>
+                <div class="col-lg-4  mt-lg-0 mt-sm-5 mt-4">
+                    <h3 class="text-light mb-3">   Contact Us</h3>
+                    <p>
+                        200, D-block, Green lane USA <br>
+                        +10 367 467 8934 <br>
+                    </p>
+                </div>
+                <div class="col-lg-2  mt-lg-0 mt-sm-5 mt-4">
+                    <h3 class="text-light mb-3"> Follow us</h3>
+                    <p class="mb-4">Follow us on social media</p>
+                    <ul class="social mt-lg-0 mt-3 ">
+                        <li class=" mr-1"><a href="#"><span class="fa fa-facebook"></span></a></li>
+                        <li class="mr-1"><a href="#"><span class="fa fa-twitter"></span></a></li>
+                        <li class="mr-1"><a href="#"><span class="fa fa-google"></span></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="copyright text-center text-light my-auto pt-3">
+                <span>Copyright &copy;وحدة التعليم الالكترونى - جامعه المنصورة </span>
+            </div>
+
+        </div>
+    </div>
+</footer>
+<!-- footer -->
+<!-- form log in-->
+<form id="test-form" class="white-popup-block mfp-hide">
+    <div class="popup_box ">
+        <div class="popup_inner">
+            <div class="logo text-center">
+                <a href="#">
+                    <h5 class="text-light">ITC Academy</h5>
+                </a>
+            </div>
+            <h3>Sign in</h3>
+            <form action="#">
+                <div class="row">
+                    <div class="col-xl-12 col-md-12">
+                        <input type="email" placeholder="Enter email">
+                    </div>
+                    <div class="col-xl-12 col-md-12">
+                        <input type="password" placeholder="Password">
+                    </div>
+                    <div class="col-xl-12">
+                        <a type="submit" href="" class="logIn">Log In</a>
+                    </div>
+                </div>
+            </form>
+            <p class="doen_have_acc">Don’t have an account? <a class="dont-hav-acc" href="#test-form2">Sign Up</a>
+            </p>
+        </div>
+    </div>
+</form>
+<!-- form login end -->
+<!-- register form-->
+<form id="test-form2" class="white-popup-block mfp-hide">
+    <div class="popup_box ">
+        <div class="popup_inner">
+            <div class="logo text-center">
+                <a href="#">
+                    <!--  <img src="img/form-logo.png" alt=""> -->
+                    <h5 class="text-light">ITC Academy</h5>
+                </a>
+            </div>
+            <h3>Resistration</h3>
+            <form action="#">
+                <div class="row">
+                    <div class="col-xl-12 col-md-12">
+                        <input type="email" placeholder="Enter email">
+                    </div>
+                    <div class="col-xl-12 col-md-12">
+                        <input type="password" placeholder="Password">
+                    </div>
+                    <div class="col-xl-12 col-md-12">
+                        <input type="Password" placeholder="Confirm password">
+                    </div>
+                    <div class="col-xl-12">
+                        <a type="submit" href="" class="logIn">Sign Up</a>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</form>
+<!-- form  end -->
+
+<!-- library for modal form-->
+<script src="/js/jquery-1.12.4.min.js"></script>
+<script src="/js/popper.min.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+<script src="/js/owl.carousel.min.js"></script>
+<script src="/js/isotope.pkgd.min.js"></script>
+<script src="/js/jquery.counterup.min.js"></script>
+<script src="/js/jquery.scrollUp.min.js"></script>
+<script src="/js/wow.min.js"></script>
+<script src="/js/jquery.slicknav.min.js"></script>
+<script src="/js/jquery.magnific-popup.min.js"></script>
+<!--contact js-->
+<script src="js/main.js"></script>
+</body>
+</html>
