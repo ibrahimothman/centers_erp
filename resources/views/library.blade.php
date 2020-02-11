118608429670d960f7ce79ed212e9527533d6f35
yasmin <yasmin@git>
2020-02-11 23:39:10 +0200

    add social media icon instead of search

--- resources/views/library.blade.php
+++ resources/views/library.blade.php
@@ -3,6 +3,10 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="description" content="">
 <meta name="author" content="">
+<!-- font awesome -->
+<link rel="stylesheet" href="/css/font-awesome.min.css">
+<!-- social media style -->
+<link rel="stylesheet" href="/css/socialMediaIcon.css">
 <!-- Custom fonts for this template-->
 <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
       rel="stylesheet">
@@ -13,8 +17,7 @@
 <link href="/../../../css/styles.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdn.rtlcss.com/bootstrap/v4.2.1/css/bootstrap.min.css"
       integrity="sha384-vus3nQHTD+5mpDiZ4rkEPlnkcyTP+49BhJ4wJeJunw06ZAp+wzzeBPUXr42fi8If" crossorigin="anonymous">
-<!-- font awesome -->
-<link rel="stylesheet" href="css/font-awesome.min.css">
+
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://cdn.rtlcss.com/bootstrap/v4.2.1/js/bootstrap.min.js"
         integrity="sha384-a9xOd0rz8w0J8zqj1qJic7GPFfyMfoiuDjC9rqXlVOcGO/dmRqzMn34gZYDTel8k"
--- resources/views/operationBar.blade.php
+++ resources/views/operationBar.blade.php
@@ -1,18 +1,66 @@
+
 <div id="content" class="operations-bar">
-
     <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-        <!-- Sidebar Toggle (Topbar) -->
-        <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3"> <i class="fa fa-bars"></i> </button>
-        <!-- Topbar Search -->
-        <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-            <div class="input-group">
-                <input type="text" class="form-control bg-light border-0 small" placeholder="البحث عن " aria-label="Search" aria-describedby="basic-addon2">
-                <div class="input-group-append">
-                    <button class="btn btn-primary" type="button"> <i class="fas fa-search fa-sm"></i> </button>
-                </div>
-            </div>
-        </form>
+       <!-- social media icon -->
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+        <div id="social">
+            <a class="facebookBtn smGlobalBtn ml-2" href="#" ></a>
+            <a class="twitterBtn smGlobalBtn mx-2"  href="#" ></a>
+            <a class="linkedinBtn smGlobalBtn mx-2" href="#" ></a>
+        </div>
+            </li>
+        </ul>
+        <!-- Topbar Navbar -->
+        <!-- Nav Item - User Information -->
+        <!-- Right Side Of Navbar -->
+        <ul class="navbar-nav ml-auto">
+            <!-- Authentication Links -->
+            @guest
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
+                @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
+                @endif
+            @else
+                <li class="nav-item dropdown no-arrow">
+                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->name }}</span>
+                        <img class="img-profile rounded-circle" src="{{url('img/user.png')}}">
+                    </a>
 
-    @include('tobbar')
+                    <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                        <a class="dropdown-item" href="#">
+                            <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                            الملف الشخصى
+                        </a>
+                        <a class="dropdown-item" href="/settings/profile">
+                            <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                            الاعدادات
+                        </a>
+
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="{{ route('logout') }}" data-toggle="modal" data-target="#logoutModal"
+                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                            خروج
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+
+                    </div>
+                </li>
+            @endguest
+        </ul>
+    </nav>
+</div>
+<!-- End of Topbar -->
+
 
 
