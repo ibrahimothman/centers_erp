eddb4f35b902cb0e6fc937c8a6a516e8a8ddc13b
ibrahimothman <ibrahimothmanre@gmail.com>
2020-03-21 22:04:48 +0200

    - Create center_middleware to check if auth user has center or not - If not redirect uesr to settings/create - Otherwise redirect user to home page - Center_middleware will apply for all routes in web.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -43,15 +43,4 @@ class LoginController extends Controller
     }
 
 
-    protected function authenticated(Request $request, $user)
-    {
-        // try to save center in session
-        if(Auth::user()->center){
-            // user has already created a center
-            Session(['center_id' => Auth::user()->center->id]);
-        }else {
-            // redirect user to centers.create
-            return redirect('settings/create');
-        }
-    }
 }
