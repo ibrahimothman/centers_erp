938adf52bcf2b1970488444fd133d68811bf8f21
ibrahimothman <ibrahimothmanre@gmail.com>
2020-03-22 20:16:14 +0200

    More in finance
    - Add finance_accounts table

---
+++ database/migrations/2020_03_22_180209_create_finance_accounts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFinanceAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('finance_accounts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->timestamps();
+
+            $table->foreign('parent_id')->references('id')->on('finance_accounts')
+                ->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('finance_accounts');
+    }
+}
