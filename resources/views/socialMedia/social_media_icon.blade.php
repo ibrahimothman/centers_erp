b7702f52aa45ed421f1b11f9c8e6a33c9d123b8a
yasmin <yasmin@git>
2020-04-06 17:46:52 +0200

    add social media folder

---
+++ resources/views/socialMedia/social_media_icon.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('library')
+    <title>social media</title>
+</head>
+<body id="page-top">
+<!-- Page Wrapper -->
+<!-- Begin Page Content -->
+<div id="wrapper">
+    @include('sidebar')
+    <div id="content-wrapper" class="d-flex flex-column">
+    @include('operationBar')
+    <!-- Begin Page Content -->
+        <section>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-2"></div>
+                    <div class="col-lg-8">
+                        <div class="card mb-4">
+                            <div class="card-header text-primary ">
+                                اضافه الروابط
+                            </div>
+
+                            <div class="card-body">
+                                <form>
+                                    <div class="form-row form-group">
+                                        <div class="col-3 ">رابط الفيسبوك
+                                        </div>
+                                        <div class="col-9 ">
+                                            <input type="url" name="" class='form-control'
+                                                   placeholder='facebook link'>
+                                        </div>
+                                    </div>
+                                    <div class="form-row form-group">
+                                        <div class="col-3 ">رابط توتير
+                                        </div>
+                                        <div class="col-9 ">
+                                            <input type="url" name="" class='form-control'
+                                                   placeholder='twitter link'>
+                                        </div>
+                                    </div>
+                                    <div class="form-row form-group">
+                                        <div class="col-3 ">رابط لينكد ان
+                                        </div>
+                                        <div class="col-9 ">
+                                            <input type="url" name="" class='form-control'
+                                                   placeholder='linked in link'>
+                                        </div>
+                                    </div>
+                                    <div class="form-row save">
+                                        <div class="col-sm-6 mx-auto" style="width: 200px;">
+                                            <button class="btn btn-primary action-buttons" type="submit" id="submit">
+                                                إضافة
+                                            </button>
+                                            <button class="btn  btn-danger action-buttons" type="reset"> إلغاء
+                                            </button>
+                                        </div>
+                                    </div>
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /.container-fluid -->
+        </section>
+        <!-- End of Main Content -->
+        @include('footer')
+    </div>
+</div>
+<!-- scroll top -->
+@include('scroll_top')
+<!-- script-->
+@include('script')
+</body>
+</html>
